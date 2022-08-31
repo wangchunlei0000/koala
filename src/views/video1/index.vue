8e37f0f2f4602e24f0ba3ecc8a769a4c254fedb0
王春雷😶‍🌫️ <2294271895@qq.com>
2022-08-31 16:48:38 +0800

    add video

--- src/views/video1/index.vue
+++ src/views/video1/index.vue
@@ -1,10 +1,94 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { VideoPlayer } from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
+
+import { Watermark } from '@/util/watermark.js'
+
+const playerOptions = ref({
+  autoplay: false,
+  muted: false,
+  loop: false,
+  preload: 'metadata',
+  language: 'en',
+  aspectRatio: '16:9',
+  fluid: true,
+  controls: true,
+  sources: [
+    {
+      src: 'https://d2zihajmogu5jn.cloudfront.net/elephantsdream/ed_hd.mp4',
+      type: 'video/mp4',
+    },
+  ],
+  bigPlayButton: false,
+  controlBar: {
+    remainingTimeDisplay: false,
+    fullscreenToggle: true,
+  },
+  playsinline: true,
+  allowsInlineMediaPlayback: false,
+})
+
+const drawWatermark = () => {
+  setTimeout(() => {
+    const realVideoElement = document.getElementsByClassName('vjs-tech')[0]
+    const exist = document.getElementById('watermark-canvas')
+    if (exist) {
+      exist.parentNode.removeChild(exist)
+    }
+    const waterMarkVideo = document.getElementById('video_watermark_element')
+    const watermarkParam = {
+      alpha: 0.6,
+      angle: -45,
+      color: '#F9C405',
+      size: 25,
+      watermarkText: 'raccoon/vue3',
+      top: 0,
+      left: 0,
+      waterWidth: realVideoElement.offsetWidth,
+      waterHeight: realVideoElement.offsetHeight,
+      offsetX: 30,
+      offsetY: 30,
+      lineY: 6,
+    }
+    const watermarkElement = new Watermark(watermarkParam).getWatermarkElement()
+    waterMarkVideo.appendChild(watermarkElement)
+  }, 100)
+}
+
+/**
+ * resize 用于重画水印
+ */
+const handleResize = () => {
+  drawWatermark()
+}
+
+onMounted(() => {
+  drawWatermark()
+  window.addEventListener('resize', handleResize, false)
+})
+</script>
 
 <template>
-  <div class="wrapper">test</div>
+  <div class="wrapper">
+    <video-player
+      ref="test"
+      id="video_watermark_element"
+      :options="playerOptions"
+      class="video-player vjs-custom-skin"
+    />
+  </div>
 </template>
 
 <style lang="less" scoped>
-.wrapper {
+.video-js {
+  .vjs-big-play-button {
+    width: 72px;
+    height: 72px;
+    border-radius: 100%;
+    z-index: 100;
+    background-color: #ffffff;
+    border: solid 1px #979797;
+  }
 }
 </style>
